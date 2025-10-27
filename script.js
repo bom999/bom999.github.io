41b08e13207a65ef04eab469bbdffebf3eda49c7
Oleg A. <83063180+bom999@users.noreply.github.com>
2025-10-27 20:20:31 +0500

    Implement URL parameter handling in script.js
    Add URL parameter extraction and redirection logic

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("JS is working!");
 });
+
+// Получаем строку запроса из текущего URL
+const params = new URLSearchParams(window.location.search);
+
+// Извлекаем значение параметра "u"
+const u = params.get('u');
+
+console.log(u); // выведет: 123
+if(u)location.href ='https://www.youtube.com/embed/' + u
